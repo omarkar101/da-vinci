46a8a8b1b15dce94f8578d2d82a8d510c53d9ce1
Omar Karazoun <59054172+omarkar101@users.noreply.github.com>
2023-09-18 20:07:53 +0300

    init project (#1)

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'da-vinci/app';
+import config from 'da-vinci/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
