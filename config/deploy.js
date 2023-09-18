7a90573db089a29eddb7737479fa9cbbd330efd2
Omar Karazoun <59054172+omarkar101@users.noreply.github.com>
2023-09-18 21:45:57 +0300

    try deploy

---
+++ config/deploy.js
@@ -0,0 +1,14 @@
+module.exports = function(deployTarget) {
+  let ENV = {
+    build: {},
+    ghpages: {
+      gitRemoteUrl: 'https://x-access-token:${DAVINCI}@github.com/omarkar101/da-vinci.git'
+    }
+  };
+
+  if (deployTarget === 'gh-pages') {
+    ENV.build.environment = 'production';
+  }
+
+  return ENV;
+};
