e639eb2f98551c2ed571731797be5d535b83125a
Omar Karazoun <59054172+omarkar101@users.noreply.github.com>
2023-09-19 13:40:42 +0300

    wip

--- app/router.js
+++ app/router.js
@@ -6,4 +6,19 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('/', function () {
+    this.route('dashboard');
+    this.route('insurance', function () {
+      this.route('shopping');
+      this.route('ongoing');
+    });
+
+    this.route('dependents-and-beneficiaries', function () {
+      this.route('beneficiaries');
+      this.route('dependents');
+    });
+    this.route('billing');
+    this.route('401k');
+  });
+});
